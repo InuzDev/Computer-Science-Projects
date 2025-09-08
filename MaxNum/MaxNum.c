ea31d303795a0e70640a41e3ca0edc7cb8227b80
InuDeveloper <daviddevlife@gmail.com>
2025-09-08 08:17:16 -0400

    Adding the update of .gitignore and MaxNum.c

--- MaxNum/MaxNum.c
+++ MaxNum/MaxNum.c
@@ -2,14 +2,16 @@
 
 int main() {
    // We now going to try and create the workflow to make the MaxNum program
-   /* 
+
+   /*
    This programs is suppose to determine what the biggest number in an array is.
    In this example, the array got a fixed size of 8 and the data is integer.
    */
-   // We need to create an array that will contian the numbers
-   int numbers[8] = {4,5,10,34,2,12,4,1}; // Arrays start from 0 index.
-   int Max = 0;
 
+   // We need to create an array that will contian the numbers
+
+   int numbers[8] = { 4, 5, 10, 34, 2, 12, 4, 1, 2 }; // Arrays start from 0 index.
+   int Max = 0;
    int length = sizeof(numbers)/sizeof(numbers[0]); // This will give us the lenght of the array
 
    for(int Index = 0; Index < length; Index++) {
