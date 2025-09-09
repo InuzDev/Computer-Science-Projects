8675c745bc45f121761738e76cab0d12cafd099a
InuDeveloper <daviddevlife@gmail.com>
2025-09-09 15:21:24 -0400

    Prettier fixes for more readability code.

--- MaxNum/MaxNum.c
+++ MaxNum/MaxNum.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
    // We now going to try and create the workflow to make the MaxNum program
 
    /*
@@ -10,12 +11,14 @@ int main() {
 
    // We need to create an array that will contian the numbers
 
-   int numbers[8] = { 4, 5, 10, 34, 2, 12, 4, 1, 2 }; // Arrays start from 0 index.
+   int numbers[8] = {4, 5, 10, 34, 2, 12, 4, 1, 2}; // Arrays start from 0 index.
    int Max = 0;
-   int numbersLength = sizeof(numbers)/sizeof(numbers[0]); // This will give us the lenght of the array
+   int numbersLength = sizeof(numbers) / sizeof(numbers[0]); // This will give us the lenght of the array
 
-   for(int Index = 0; Index < numbersLength; Index++) {
-      if(numbers[Index] > Max) {
+   for (int Index = 0; Index < numbersLength; Index++)
+   {
+      if (numbers[Index] > Max)
+      {
          Max = numbers[Index];
       }
    }
