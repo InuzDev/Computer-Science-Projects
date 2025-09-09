803c038d6f7569989e837d8368cc2e2ad281c940
InuDeveloper <daviddevlife@gmail.com>
2025-09-09 15:20:53 -0400

    Prettier fixes for more readability code.

--- SumOddsAndEvens/OddsAndEvens.c
+++ SumOddsAndEvens/OddsAndEvens.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 
-int main() {
-   
-   //    Hallar la suma de los números pares e impares comprendidos en un rango de números 
-   // 	Ejemplo 
-   // 	Rango 1, 15 
-   // 	Suma de los pares: (2+4+6+8+10+12+14) = 56 
-   // 	Suma de los impares: (1,3,5,7,9,11,13,15) = 6
-   int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-   int arrlength = sizeof(arr)/sizeof(arr[0]);
+int main()
+{
+
+   //    Hallar la suma de los números pares e impares comprendidos en un rango de números
+   // 	Ejemplo
+   // 	Rango 1, 15
+   // 	Suma de los pares: (2+4+6+8+10+12+14) = 56
+   // 	Suma de los impares: (1,3,5,7,9,11,13,15) = 64
+   int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+   int arrlength = sizeof(arr) / sizeof(arr[0]);
 
    int SumEven = 0; // This store the result of the sum of even numbers.
    int SumOdd = 0;  // This store the result of the sum of odd numbers.
-   for(int i = 0; i < arrlength; i++){
-      if(arr[i] % 2 == 0){
+   for (int i = 0; i < arrlength; i++)
+   {
+      if (arr[i] % 2 == 0) // Check if the divisor equal 0.
+      {
          SumEven += arr[i];
-      } else {
+      }
+      else
+      {
          SumOdd += arr[i];
       }
    }
