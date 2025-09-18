7f1c1635181734531e405325e4faf52aad3ee815
InuDeveloper <daviddevlife@gmail.com>
2025-09-18 14:26:58 -0400

    Working in a Intro. to algorithm homework

---
+++ C/MaxNum/MaxNum.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+   // We now going to try and create the workflow to make the MaxNum program
+
+   /*
+   This programs is suppose to determine what the biggest number in an array is.
+   In this example, the array got a fixed size of 8 and the data is integer.
+   */
+
+   // We need to create an array that will contian the numbers
+
+   int numbers[9] = {4, 5, 10, 34, 2, 12, 4, 1, 2}; // Arrays start from 0 index.
+   int Max = 0;
+   int numbersLength = sizeof(numbers) / sizeof(numbers[0]); // This will give us the lenght of the array
+
+   for (int Index = 0; Index < numbersLength; Index++)
+   {
+      if (numbers[Index] > Max)
+      {
+         Max = numbers[Index];
+      }
+   }
+
+   printf("%d \n", Max); // Keep this, so we receive an output.
+
+   return 0;
+}
---
+++ C/PrimeNumber/PrimeNumber.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+int main()
+{
+   // Optimized approach - Thanks to GeeksforGeeks
+   int n = 21;
+   int cnt = 0;
+
+   if (n <= 1)
+   {
+      printf("%d is NOT prime", n);
+   }
+   else
+   {
+      // Check how many numbers divide n in
+      // range 2 to sqrt(n)
+      for (int i = 2; i * i <= n; i++)
+      {
+         if (n % i == 0)
+         {
+            cnt++;
+         }
+      }
+
+      // if cnt is greater than 0 then n is not prime
+      if (cnt > 0)
+      {
+         printf("%d is NOT prime", n);
+      }
+      else
+      {
+         printf("%d is prime", n);
+      }
+   }
+
+   return 0;
+}
