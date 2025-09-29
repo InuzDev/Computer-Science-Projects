6c82fd7568274dd7b6e1ec9222fe79059cd9ccf1
InuDeveloper <daviddevlife@gmail.com>
2025-09-29 12:18:31 -0400

    Added a code I made before

---
+++ C/ModularProgram_Test/Index.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// Como hacer un programa modular en c
+
+void HelloWorld(char* StringWorld) {
+  printf("%s", StringWorld);
+}
+
+int main() {
+  HelloWorld("Hello World");
+  return 0;
+}
