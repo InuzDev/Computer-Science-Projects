c0dc32a6c017bbcd779c959e9a368b639b46b47c
InuDeveloper <daviddevlife@gmail.com>
2025-09-30 15:05:56 -0400

    Removed a file made with vim, going to rework it later

--- ShortCutKeyboard.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-// This file is made to learn about the shortcuts of the keyboard, I may have created this file in the wrong directory
-
-int main()
-{
-   printf("Testing this program, I'm going to modulirze and rise it from ground up.")
-}
