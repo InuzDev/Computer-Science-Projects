4fc648c5a28199c57bd9dcbbd9d06b237cab93a5
InuDeveloper <daviddevlife@gmail.com>
2025-11-18 17:26:03 -0400

    Added matrix project to the repositiory ; Pending documentation

--- C/ClassPractice/Matrix/index.c
+++ C/ClassPractice/Matrix/index.c
@@ -1,10 +1,445 @@
 /**
- * Matrix Exercise for ICC - PUCMM
+ * Objective: A modular program that ask the dimension of a square matrix, maximum being 15
+ *            and minimum is 3, Generate the matrix with random numbers between two values indicated by the user
+ *            and print an expanded matrix, indicating the sumatory and average of the values in each column and row,
+ *            total sumatory of the values from the matrix, average of all values, minimum and maximum value in the matrix
+ *
+ * ! Important note: ! do NOT modify the original matrix, or making a new one. JUST print the indicated values
+ *
+ * Programa para practicar matrices
+ *
+ * InuDev ; 16 - 11 - 2025
+ *
+ * Archivo: main.c
  */
+#include <conio.c>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#define MIN_DIM 3
+#define MAX_DIM 15
+
+#define MAX_VALUE 9999
+
+#define POSXINI 10
+#define POSYINI 5
+
+#define CT BLUE
+#define CF LIGHTGRAY
+
+#define CTS BLUE
+#define CFS YELLOW
+
+#define CTM YELLOW
+#define CFM GREEN
+
+#define ESP 6
+
+#define ENTER 13
+#define ESC 27
+
+#define UP 72
+#define DOWN 80
+#define RIGHT 77
+#define LEFT 75
+
+#define NOMARK -1
+
+#define KEY_PREFIX_1 0
+#define KEY_PREFIX_2 224
+
+#define CURSOR_0 0
+#define CURSOR_100 100
+
+void genMatrix(int MinRange, int MaxRange, int dimensions, int Matrix[dimensions][dimensions]);
+void showMatrix(int dimensions, int Matrix[dimensions][dimensions], int RowMark, int ColMark, int RowSel, int ColSel, int posX, int posY);
+void drawCell(int dimensions, int Matrix[dimensions][dimensions], int RowMark, int ColMark, int RowSel, int ColSel, int row, int col, int posX, int posY);
+void moveMatrix(int dimensions, int Matrix[dimensions][dimensions], int posX, int posY);
+int maxMatrix(int dimensions, int Matrix[dimensions][dimensions]);
+
+void colordefault(void);
+void setcolor(int, int);
+
+int randrange(int, int);
 
 int main()
 {
+   srand(time(NULL));
+   int dimensions = 0, MinRange = -1, MaxRange = -1;
+
+   while (1)
+   {
+      printf("Introduzca las dimensiones de la matriz: ");
+      scanf("%d", &dimensions);
+      if (dimensions > MAX_DIM || dimensions < MIN_DIM)
+      {
+         printf("Valor introducido invalido. Por, favor. Introduzca un valor entre [%d, %d]\n\n", MIN_DIM, MAX_DIM);
+         continue;
+      }
+      break;
+   }
+
+   int Matrix[dimensions][dimensions];
+
+   while (1)
+   {
+      printf("Ingresa el rango inferior para generar los numeros aleatorios de la matriz: ");
+      scanf("%d", &MinRange);
+      if (MinRange < 0)
+      {
+         printf("Por, favor. Ingresar un valor mayor o igual a 0\n\n");
+         continue;
+      }
+      break;
+   }
+
+   while (1)
+   {
+      printf("Ingresa el rango superior para generar los numeros aleatorios de la matriz: ");
+      scanf("%d", &MaxRange);
+      if (MaxRange < MinRange || MaxRange > MAX_VALUE)
+      {
+         printf("Por, favor. Ingresar un valor mayor al rango minimo.\n\n");
+         continue;
+      }
+      break;
+   }
+
+   system("cls");
+
+   genMatrix(MinRange, MaxRange, dimensions, Matrix);
+   moveMatrix(dimensions, Matrix, POSXINI, POSYINI);
+
+   int maximum = maxMatrix(dimensions, Matrix);
+
    return 0;
+}
+
+/**
+ * Funcion: randrange
+ * Objetivo: Generar los valores aleatorios para la matriz
+ * Argumentos: MinRange, MaxRange
+ * Retorna: valores aleatorios para la matrix a base de la semilla que utiliza el tiempo. (Cambia cada segundo)
+ */
+int randrange(int MinRange, int MaxRange)
+{
+   return rand() % (MaxRange - MinRange + 1) + MinRange;
+}
+
+/**
+ * Funcion: maxMatrix
+ * Objetivo: Encontrar el maximo valor en una matriz
+ * Argumentos: (int) dimensions, posX, posY Matriz[dimensions][dimensions]
+ * retorna: El maximo valor de la matriz.
+ */
+int maxMatrix(int dimensions, int Matrix[dimensions][dimensions])
+{
+   int maxVal = Matrix[0][0];
+
+   for (int Xindex = 0; Xindex < dimensions; Xindex++)
+   {
+      for (int Yindex = 0; Yindex < dimensions; Yindex++)
+      {
+         if (maxVal < Matrix[Xindex][Yindex])
+         {
+            maxVal = Matrix[Xindex][Yindex];
+         }
+      }
+   }
+
+   return maxVal;
+}
+
+/**
+ * Funcion: GenMatrix
+ * Objetivo: Generar la matriz con los valores aleatorios utilizando la funcion randrange
+ * Argumentos: (int) MinRange, MaxRange, dimensions, Matrix[dimensions][dimensions]
+ */
+void genMatrix(int MinRange, int MaxRange, int dimensions, int Matrix[dimensions][dimensions])
+{
+   for (int Xindex = 0; Xindex < dimensions; Xindex++)
+   {
+      for (int Yindex = 0; Yindex < dimensions; Yindex++)
+      {
+         Matrix[Xindex][Yindex] = randrange(MinRange, MaxRange);
+      }
+   }
+}
+
+/**
+ * Funcion drawCell
+ * Objetivo: Dibujar la tabla cada vez que se mueve el usuario evitando bugs de grafica.
+ * Argumentos: (int) dimensions, Matrix[dimensions][dimensions] RowMark ColMark, RowSel, ColSel, row, col, posX, posY
+ */
+void drawCell(int dimensions, int Matrix[dimensions][dimensions], int RowMark, int ColMark, int RowSel, int ColSel, int row, int col, int posX, int posY)
+{
+   int isSelected = (row == RowSel && col == ColSel);
+   int isMarked = (row == RowMark || col == ColMark);
+
+   gotoxy(posX + col * ESP, posY + row);
+   if (isSelected)
+      setcolor(CTS, CFS);
+   else if (isMarked)
+      setcolor(CTM, CFM);
+   else
+      setcolor(CT, CF);
+
+   printf("%4d", Matrix[row][col]);
+   colordefault();
+}
+
+/**
+ * funcion: ShowMatrix
+ * Objetivo: Mostrar la matriz
+ * Argumentos: (int) dimension,s Matrix[dimensions][dimensions], RowSel, posX, ColSel, posY
+ */
+void showMatrix(int dimensions, int Matrix[dimensions][dimensions], int RowMark, int ColMark, int RowSel, int ColSel, int posX, int posY)
+{
+   int rowSum[dimensions];
+   float rowAvg[dimensions];
+   int colSum[dimensions];
+   float colAvg[dimensions];
+   int totalSum = 0;
+   int minVal = Matrix[0][0];
+   int maxVal = Matrix[0][0];
+
+   for (int index = 0; index < dimensions; index++)
+      colSum[index] = 0;
+
+   for (int index = 0; index < dimensions; index++)
+   {
+      int value = 0;
+      for (int index = 0; index < dimensions; index++)
+      {
+         int Value = Matrix[index][index];
+         value += Value;
+         colSum[index] += Value;
+         if (Value < minVal)
+            minVal = Value;
+         if (Value > maxVal)
+            maxVal = Value;
+      }
+      rowSum[index] = value;
+      rowAvg[index] = (float)value / (float)dimensions;
+      totalSum += value;
+   }
+
+   for (int Index = 0; Index < dimensions; Index++)
+      colAvg[Index] = (float)colSum[Index] / (float)dimensions;
+
+   float overallAvg = (float)totalSum / (float)(dimensions * dimensions);
+
+   /* Column headers */
+   gotoxy(posX - ESP, posY - 2);
+   printf("     ");
+   for (int Index = 0; Index < dimensions; Index++)
+      printf("%*d", ESP, Index + 1);
+
+   printf("%*s%*s", ESP, "", ESP, "");
+
+   for (int Index = 0; Index < dimensions; Index++)
+   {
+      gotoxy(posX - ESP, posY + Index);
+      printf("R%02d", Index + 1);
+
+      for (int _index = 0; _index < dimensions; _index++)
+      {
+         drawCell(dimensions, Matrix, RowMark, ColMark, RowSel, ColSel, Index, _index, posX, posY);
+      }
+
+      gotoxy(posX + dimensions * ESP + 2, posY + Index);
+      setcolor(CTM, CFM);
+      printf(" %6d %6.2f", rowSum[Index], rowAvg[Index]);
+      colordefault();
+   }
+
+   gotoxy(posX - ESP, posY + dimensions + 0);
+   printf("SUM");
+   for (int c = 0; c < dimensions; c++)
+   {
+      gotoxy(posX + c * ESP, posY + dimensions + 0);
+      setcolor(CTM, CFM);
+      printf("%6d", colSum[c]);
+      colordefault();
+   }
+
+   gotoxy(posX - ESP, posY + dimensions + 1);
+   printf("AVG");
+   for (int c = 0; c < dimensions; c++)
+   {
+      gotoxy(posX + c * ESP, posY + dimensions + 1);
+      setcolor(CTM, CFM);
+      printf("%6.2f", colAvg[c]);
+      colordefault();
+   }
+
+   gotoxy(posX + dimensions * ESP + 2, posY + dimensions + 0);
+   setcolor(CTS, CFS);
+   printf("Total:%8d", totalSum);
+   gotoxy(posX + dimensions * ESP + 2, posY + dimensions + 1);
+   printf("Overall Avg:%6.2f", overallAvg);
+   gotoxy(posX + dimensions * ESP + 2, posY + dimensions + 2);
+   printf("Min:%6d  Max:%6d", minVal, maxVal);
+   colordefault();
+}
+
+/**
+ * Funcion: MoveMatrix
+ * Objetivo: Permitir al usaurio moverse en la matriz
+ * Argumentos: (int) dimensions, Matrix[dimensions][dimensions], posX, posY
+ */
+void moveMatrix(int dimensions, int Matrix[dimensions][dimensions], int posX, int posY)
+{
+   int RowSel = 0;
+   int ColSel = 0;
+   int RowMark = NOMARK;
+   int ColMark = NOMARK;
+   int newRow = 0;
+   int newCol = 0;
+   int swapTemp = 0;
+   int inputKey = 0;
+
+   _setcursortype(CURSOR_0);
+
+   showMatrix(dimensions, Matrix, RowMark, ColMark, RowSel, ColSel, posX, posY);
+
+   int prevRow = RowSel, prevCol = ColSel;
+
+   do
+   {
+      inputKey = getch();
+      if (inputKey == KEY_PREFIX_1 || inputKey == KEY_PREFIX_2 || inputKey == 224)
+         inputKey = getch();
+
+      int fullRedraw = 0;
+
+      if (inputKey == UP)
+      {
+         if (RowMark == NOMARK)
+         {
+            RowSel = (RowSel == 0) ? dimensions - 1 : RowSel - 1;
+         }
+         else
+         {
+            newRow = (RowMark == 0) ? dimensions - 1 : RowMark - 1;
+            for (int colIndex = 0; colIndex < dimensions; colIndex++)
+            {
+               swapTemp = Matrix[RowMark][colIndex];
+               Matrix[RowMark][colIndex] = Matrix[newRow][colIndex];
+               Matrix[newRow][colIndex] = swapTemp;
+            }
+            RowMark = newRow;
+            RowSel = RowMark;
+            fullRedraw = 1;
+         }
+      }
+      else if (inputKey == DOWN)
+      {
+         if (RowMark == NOMARK)
+         {
+            RowSel = (RowSel == dimensions - 1) ? 0 : RowSel + 1;
+         }
+         else
+         {
+            newRow = (RowMark == dimensions - 1) ? 0 : RowMark + 1;
+            for (int colIndex = 0; colIndex < dimensions; colIndex++)
+            {
+               swapTemp = Matrix[RowMark][colIndex];
+               Matrix[RowMark][colIndex] = Matrix[newRow][colIndex];
+               Matrix[newRow][colIndex] = swapTemp;
+            }
+            RowMark = newRow;
+            RowSel = RowMark;
+            fullRedraw = 1;
+         }
+      }
+      else if (inputKey == LEFT)
+      {
+         if (ColMark == NOMARK)
+         {
+            ColSel = (ColSel == 0) ? dimensions - 1 : ColSel - 1;
+         }
+         else
+         {
+            newCol = (ColMark == 0) ? dimensions - 1 : ColMark - 1;
+            for (int rowIndex = 0; rowIndex < dimensions; rowIndex++)
+            {
+               swapTemp = Matrix[rowIndex][ColMark];
+               Matrix[rowIndex][ColMark] = Matrix[rowIndex][newCol];
+               Matrix[rowIndex][newCol] = swapTemp;
+            }
+            ColMark = newCol;
+            ColSel = ColMark;
+            fullRedraw = 1;
+         }
+      }
+      else if (inputKey == RIGHT)
+      {
+         if (ColMark == NOMARK)
+         {
+            ColSel = (ColSel == dimensions - 1) ? 0 : ColSel + 1;
+         }
+         else
+         {
+            newCol = (ColMark == dimensions - 1) ? 0 : ColMark + 1;
+            for (int rowIndex = 0; rowIndex < dimensions; rowIndex++)
+            {
+               swapTemp = Matrix[rowIndex][ColMark];
+               Matrix[rowIndex][ColMark] = Matrix[rowIndex][newCol];
+               Matrix[rowIndex][newCol] = swapTemp;
+            }
+            ColMark = newCol;
+            ColSel = ColMark;
+            fullRedraw = 1;
+         }
+      }
+      else if (inputKey == ENTER)
+      {
+         int newValue = Matrix[RowSel][ColSel];
+         gotoxy(posX, posY + dimensions + 4);
+         printf("Ingrese nuevo valor para R%dC%d: ", RowSel + 1, ColSel + 1);
+         fflush(stdout);
+         if (scanf("%d", &newValue) == 1)
+         {
+            Matrix[RowSel][ColSel] = newValue;
+            fullRedraw = 1;
+         }
+         gotoxy(posX, posY + dimensions + 4);
+         printf("%*s", 60, "");
+         gotoxy(posX, posY + dimensions + 4);
+      }
+
+      if (fullRedraw)
+      {
+         showMatrix(dimensions, Matrix, RowMark, ColMark, RowSel, ColSel, posX, posY);
+         prevRow = RowSel;
+         prevCol = ColSel;
+      }
+      else
+      {
+         if (prevRow != RowSel || prevCol != ColSel)
+         {
+            drawCell(dimensions, Matrix, RowMark, ColMark, RowSel, ColSel, prevRow, prevCol, posX, posY);
+            drawCell(dimensions, Matrix, RowMark, ColMark, RowSel, ColSel, RowSel, ColSel, posX, posY);
+            prevRow = RowSel;
+            prevCol = ColSel;
+         }
+      }
+
+   } while (inputKey != ESC);
+
+   _setcursortype(CURSOR_100);
+}
+
+void setcolor(int ct, int cf)
+{
+   textcolor(ct);
+   textbackground(cf);
+}
+
+void colordefault(void)
+{
+   setcolor(LIGHTGRAY, BLACK);
 }
--- C/ClassPractice/memoryGame/index.c
+++ C/ClassPractice/memoryGame/index.c
@@ -2,30 +2,166 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <conio.c>
 
 #define DIM 6
+#define VALINI 0
+#define ESP 3
+#define MAXCHRS 27
 
-#define VAL_INI 3
-#define VAL_END 3225
+#define CT BLUE
+#define CF LIGHTGRAY
 
-#define CANTDIG(X) (int)log10(X) + 1
+#define CTS BLUE
+#define CFS YELLOW
 
+#define CTM YELLOW
+#define CFM GREEN
+
+#define POSXINI 10
+#define POSYINI 5
+
+#define ENTER 13
+#define ESC 27
+
+#define ARRIBA 72
+#define ABAJO 80
+#define DERECHA 77
+#define IZQUIERDA 75
+
+void genmat(int cantfil, int cantcol, int mat[cantfil][cantcol], char figuras[]);
+void showmatxy(int cantfil, int cantcol, int mat[cantfil][cantcol], int filsel, int colsel, int posx, int posy);
+void movematxy(int cantfil, int cantcol, int mat[cantfil][cantcol], int posx, int posy);
+void setcolor(int, int);
+void colordefault(void);
 int randrange(int, int);
-int maxMatrix(int cantfil, int cantcol, int matrix[cantfil][cantcol]);
-int traceMatrix(int cantfil, int cantcol, int matrix[cantfil][cantcol]);
-void genMatrix(int cantfil, int cantcol, int matrix[cantfil][cantcol], int, int);
-void showMatrix(int cantfil, int cantcol, int matrix[cantfil][cantcol]);
 
 int main()
 {
-   int figures[] = {1, 2, 3, 4, 5, 6, 15, 16, 17, 18, 127, 143, 224, 225, 231, 232, 233, 234, 235, 236, 237, 238, 0};
-
-   printf("%c", figures);
-   return 0;
 
    srand(time(NULL));
 
+   char figuras[MAXCHRS] = {64, 128, 142, 143, 146, 152, 153, 154, 155, 156, 157, 158, 159, 224, 225, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238};
+   int mat[DIM][DIM];
+
+   /* for ( int val = 1; val <= 255; val++ )
+       printf("%d - %c\n",val,val);
+
+    printf("%s",figuras);
+    getch();*/
+
+   genmat(DIM, DIM, mat, figuras);
+   system("cls");
+   printf("Valores capturados:\n");
+   movematxy(DIM, DIM, mat, POSXINI, POSYINI);
    return 0;
+}
+
+void initmat(int cantfil, int cantcol, int mat[cantfil][cantcol], int valini)
+{
+   for (int indfil = 0; indfil < cantfil; indfil++)
+      for (int indcol = 0; indcol < cantcol; indcol++)
+         mat[indfil][indcol] = valini;
+
+   return;
+}
+
+void genmat(int cantfil, int cantcol, int mat[cantfil][cantcol], char figuras[])
+{
+   int indfiguras, indfilasign, indcolasign, cantasign = 0;
+   char chr;
+
+   initmat(cantfil, cantcol, mat, VALINI);
+
+   for (int indfil = 0; indfil < cantfil; indfil++)
+      for (int indcol = 0; indcol < cantcol; indcol++)
+      {
+         do
+         {
+            indfiguras = randrange(0, MAXCHRS - 1);
+
+         } while (figuras[indfiguras] == VALINI);
+
+         chr = figuras[indfiguras];
+         if (mat[indfil][indcol] == VALINI)
+         {
+            mat[indfil][indcol] = chr;
+            do
+            {
+               indfilasign = randrange(0, cantfil);
+               indcolasign = randrange(0, cantcol);
+            } while (mat[indfilasign][indcolasign] != VALINI);
+            mat[indfilasign][indcolasign] = chr;
+            figuras[indfiguras] = VALINI;
+            cantasign++;
+            if (cantasign == (cantfil * cantcol / 2))
+               break;
+         }
+      }
+
+   return;
+}
+
+void showmatxy(int cantfil, int cantcol, int mat[cantfil][cantcol], int filsel, int colsel, int posx, int posy)
+{
+   for (int indfil = 0; indfil < cantfil; indfil++)
+   {
+      for (int indcol = 0; indcol < cantcol; indcol++)
+      {
+         gotoxy(posx + indcol * ESP, posy + indfil);
+         setcolor(CT, CF);
+         if (indfil == filsel && indcol == colsel)
+            setcolor(CTS, CFS);
+
+         printf(" %c ", mat[indfil][indcol]);
+      }
+   }
+   colordefault();
+   return;
+}
+
+void movematxy(int cantfil, int cantcol, int mat[cantfil][cantcol], int posx, int posy)
+{
+   int filsel = 0, colsel = 0;
+   int tecla;
+
+   _setcursortype(0);
+   do
+   {
+      showmatxy(cantfil, cantcol, mat, filsel, colsel, posx, posy);
+      do
+      {
+         tecla = getch();
+      } while (tecla != ENTER && tecla != ESC && tecla != ARRIBA && tecla != ABAJO &&
+               tecla != DERECHA && tecla != IZQUIERDA);
+
+      if (tecla == ARRIBA)
+      {
+         filsel--;
+         if (filsel < 0)
+            filsel = cantfil - 1;
+      }
+      if (tecla == DERECHA)
+      {
+         colsel++;
+         if (colsel == cantcol)
+            colsel = 0;
+      }
+      if (tecla == ABAJO)
+      {
+         filsel++;
+         if (filsel == cantfil)
+            filsel = 0;
+      }
+      if (tecla == IZQUIERDA)
+      {
+         colsel--;
+         if (colsel < 0)
+            colsel = cantcol - 1;
+      }
+
+   } while (tecla != ESC);
+   _setcursortype(100);
 }
 
 int randrange(int liminf, int limsup)
@@ -33,84 +169,13 @@ int randrange(int liminf, int limsup)
    return rand() % (limsup - liminf + 1) + liminf;
 }
 
-int sumMatrix(int cantfil, int cantcol, int matrix[cantfil][cantcol])
+void setcolor(int ct, int cf)
 {
-   int total = 0;
-
-   for (int indexFil = 0; indexFil < cantfil; indexFil++)
-   {
-      for (int indexCol = 0; indexCol < cantcol; indexCol++)
-      {
-         total += matrix[indexFil][indexCol];
-      }
-   }
+   textcolor(ct);
+   textbackground(cf);
 }
 
-int maxMatrix(int cantfil, int cantcol, int matrix[cantfil][cantcol])
+void colordefault(void)
 {
-   int maxVal = matrix[0][0];
-
-   for (int indexFil = 0; indexFil < cantfil; indexFil++)
-   {
-      for (int indexCol = 0; indexCol < cantcol; indexCol++)
-      {
-         if (maxVal < matrix[indexFil][indexCol])
-         {
-            maxVal = matrix[indexFil][indexCol];
-         }
-      }
-   }
-}
-
-int traceMatrix(int cantfil, int cantcol, int matrix[cantfil][cantcol])
-{
-   if (cantfil != cantcol)
-   {
-      return -1;
-   }
-
-   int valTrace = 0;
-
-   for (int index = 0; index < cantfil; index++)
-   {
-      valTrace += matrix[index][index];
-   }
-
-   return valTrace;
-}
-
-void getMatrix(int cantfil, int cantcol, int matrix[cantfil][cantcol])
-{
-   for (int indexFil = 0; indexFil < cantfil; indexFil++)
-   {
-      for (int indexCol = 0; indexCol < cantcol; indexCol++)
-      {
-         printf("Val[%d][%d]: ", indexFil + 1, indexCol + 1);
-         scanf("%d", &matrix[indexFil][indexCol]);
-      }
-   }
-}
-
-void genMatrix(int cantfil, int cantcol, int matrix[cantfil][cantcol], int, int)
-{
-   for (int indexFil = 0; indexFil < cantfil; indexFil++)
-   {
-      for (int indexCol = 0; indexCol < cantcol; indexCol++)
-      {
-         matrix[indexFil][indexCol] = randrange(VAL_INI, VAL_END);
-      }
-   }
-}
-
-void showMatrix(int cantfil, int cantcol, int matrix[cantfil][cantcol])
-{
-   int cantDig = (int)log10(maxMatrix(cantfil, cantcol, matrix)) + 1;
-   for (int indexFil = 0; indexFil < cantfil; indexFil++)
-   {
-      for (int indexCol = 0; indexCol < cantcol; indexCol++)
-      {
-         printf("%*d ", cantDig, matrix[indexFil][indexCol]);
-      }
-      printf("\n");
-   }
+   setcolor(LIGHTGRAY, BLACK);
 }
