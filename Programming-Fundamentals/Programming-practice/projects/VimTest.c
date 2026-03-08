1c7e1927a093aa6ed2f17e151eb5fc3ed14d29a3
InuDeveloper <daviddevlife@gmail.com>
2026-03-08 14:33:45 -0400

    Working and testing Vim editor, added VimTest.c file

---
+++ Programming-Fundamentals/Programming-practice/projects/VimTest.c
@@ -0,0 +1,15 @@
+/*
+ * VimTest.c
+ *
+ * This is a test program testing how lazy vim feels and how it works.
+ */
+
+#include <conio.c>
+#include <stdio.h>
+
+int main() {
+   printf("Recursive practice: ");
+
+   // Here, I will practice the recursivity in many functions.
+   return 0;
+}
