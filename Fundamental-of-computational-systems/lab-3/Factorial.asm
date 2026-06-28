70c386f099b5e6c59127cc3fa305dea5ce9aec4e
InuDeveloper <daviddevlife@gmail.com>
2026-06-27 21:52:02 -0400

    Updated Assembly programs, using the custom lib I created

--- Fundamental-of-computational-systems/lab-3/Factorial.asm
+++ Fundamental-of-computational-systems/lab-3/Factorial.asm
@@ -1,112 +1,34 @@
 ; Ejercicio 2 ; Factorial de un numero
+
+%include "linux64io.inc"
+
 section .data
    msg_in db "Ingrese un numero: "
    msg_in_len equ $ - msg_in
    msg_res db "El factorial es: "
    msg_res_len equ $ - msg_res
 
-section .bss
-   inbuf resb 20
-   outbuf resb 25
-
 section .text
    global _start
 
 _start:
-   ; Aqui lee el numero
-   mov rax, 1
-   mov rdi, 1
-   mov rsi, msg_in
-   mov rdx, msg_in_len
-   syscall
+   PRINT_STR msg_in, msg_in_len
+   READ_INT r12
 
-   mov rax, 0
-   mov rdi, 0
-   mov rsi, inbuf
-   mov rdx, 20
-   syscall
-
-   mov rsi, inbuf
-   call str_to_int
-   mov r12, rax  ; r12 = n
-
-   ; Ahora vamos a calcular el factorial
-   ; El resultadoo empieza en 1, multiplaremos por n, n-1, n-2, hasta llegar a 1.
-   mov r13, 1 ; r13 = result
-   mov rcx, r12 ; contador = n
+   mov r13, 1
+   mov rcx, r12
 
 .loop:
    cmp rcx, 1
-   jle .done
+   jle .end
    imul r13, rcx
    dec rcx
    jmp .loop
 
-.done:
-   ; Mostraremos el resultado ahora
-   mov rax, 1
-   mov rdi, 1
-   mov rsi, msg_res
-   mov rdx, msg_res_len
-   syscall
-
-   mov rax, r13
-   call print_int
+.end:
+   PRINT_STR msg_res, msg_res_len
+   PRINT_INT r13
 
    mov rax, 60
    xor rdi, rdi
    syscall
-
-; str_to_int: Convierte string de digitos en rsi a entero en rax
-str_to_int:
-   xor rax, rax
-   xor rcx, rcx
-
-.loop:
-   movzx rcx, byte [rsi]
-   cmp cl, '0'
-   jl .done
-   cmp cl, '9'
-   jg .done
-   sub cl, '0'
-   imul rax, rax, 10
-   add rax, rcx
-   inc rsi
-   jmp .loop
-.done:
-   ret
-
-; print_int: imprimiremos ele ntero en rax seguido de un salto de linea
-print_int:
-   lea r9, [outbuf + 24]
-   mov byte [r9], 0x0a
-   dec r9
-   mov r10, 1
-
-   test rax, rax
-   jnz .digits
-   mov byte [r9], '0'
-   dec r9
-   inc r10
-   jmp .write
-
-.digits:
-   test rax, rax
-   jz .write
-   xor rdx, rdx
-   mov rbx, 10
-   div rbx
-   add dl, '0'
-   mov [r9], dl
-   dec r9
-   inc r10
-   jmp .digits
-
-.write:
-   inc r9
-   mov rax, 1
-   mov rdi, 1
-   mov rsi, r9
-   mov rdx, r10
-   syscall
-   ret
--- Fundamental-of-computational-systems/lab-3/NumPrimo.asm
+++ Fundamental-of-computational-systems/lab-3/NumPrimo.asm
@@ -1,6 +1,8 @@
 ; meow meow meow
 ; Ejercicio 3 ; Verificar si un numero es primo
 
+%include "linux64io.inc"
+
 section .data
    msg_in db "Ingrese un numero: "
    msg_in_len equ $ - msg_in
@@ -9,95 +11,42 @@ section .data
    msg_no_primo db "No es un numero primo", 0x0a
    msg_no_primo_len equ $ - msg_no_primo
 
-section .bss
-   inbuf resb 20
-
 section .text
    global _start
 
 _start:
-   ; Aqui leemos el numero
-   mov rax, 1
-   mov rdi, 1
-   mov rsi, msg_in
-   mov rdx, msg_in_len
-   syscall
+   PRINT_STR msg_in, msg_in_len
+   READ_INT r12
 
-   mov rax, 0
-   mov rdi, 0
-   mov rdi, inbuf
-   mov rsi, msg_in
-   mov rdx, msg_in_len
-   syscall
-
-   mov rax, 0
-   mov rdi, 0
-   mov rsi, inbuf
-   mov rdx, 20
-   syscall
-
-   mov rsi, inbuf
-   call str_to_int
-   mov r12, rax ; r12 = n
-
-   ; Si el numero es menor o igual a 1, no son primos
+   ; Verificar si el valor ingresado es 1 o menor.
    cmp r12, 1
    jle .no_primo
 
-   ; Probamos divisores desde 2 hasta n-1
-   mov r13, 2 ; r13 = divisor actual
+   mov r13, 2
 
 .loop:
-   cmp r13, r12 ; Si divisor >= n, es primo
+   cmp r13, r12
 
    jge .es_primo
 
    mov rax, r12
    xor rdx, rdx
-   div r13 ; rdx = n % r13
+   div r13
 
-   test rdx, rdx ; Si el residuo es 0, no es primo
+   test rdx, rdx
    jz .no_primo
 
    inc r13
    jmp .loop
 
 .es_primo:
-   mov rax, 1
-   mov rdi, 1
-   mov rsi, msg_primo
-   mov rdx, msg_primo_len
-   syscall
-   jmp .salir
+   PRINT_STR msg_primo, msg_primo_len
+   jmp .end
 
 .no_primo:
-   mov rax, 1
-   mov rdi, 1
-   mov rsi, msg_no_primo
-   mov rdx, msg_no_primo_len
-   syscall
+   PRINT_STR msg_no_primo, msg_no_primo_len
 
-.salir:
+.end:
    mov rax, 60
    xor rdi, rdi
    syscall
-
-; str_to_int: Esto lo que hace convertir un string de digitos en rsi a entero en rax
-str_to_int:
-   xor rax, rax
-   xor rcx, rcx
-
-.loop:
-   movzx rcx, byte [rsi]
-   cmp cl, '0'
-   jl .done
-   cmp cl, '9'
-   jg .done
-   sub cl, '0'
-   imul rax, rax, 10
-   add rax, rcx
-   inc rsi
-   jmp .loop
-
-.done:
-   ret
